7e39cf359aaa305e27bf1b05f6707cdbbaaecd1c
apoorvaverma20 <apoorvaverma20@gmail.com>
2019-09-23 17:29:22 +0530

    add

--- server_client/client1.c
+++ server_client/client1.c
@@ -19,16 +19,16 @@ void func(int sockfd)
        char arr[100];
        //char value;
        bzero(arr, MAX);
-       enum command{Read,writing,add,delete,update}; //0 to 4
+       enum command{Read=0,writing=1,remove=2,edit=3}; //0 to 3
        enum attri_id{dev_name=0,loc=1,manf_name=2};
        int8_t dev_id;
        int x;
        int8_t attr_len;
       // char dev_name,loc,manf_name;
-       char name[10];
+       char name[31];
        enum command k;
        enum attri_id m;
-      // m=0;
+      
          for(;;)
        {
          printf("1.Add a device\n");
@@ -37,15 +37,12 @@ void func(int sockfd)
          printf("Enter the value of x : \n");
          scanf("%d",&x);
      
-       
-       switch(x)
-       {
-       case 1:      //add a device
+       if(x==1)
        {
         arr[i]=writing;    //command type                //arr[0]
         printf("writing means %d\n",arr[i]);
         i=i+1;
-        printf("value of i id %d\n",i);
+        printf("value of i is %d\n",i);
 
         printf("Enter the deviceID\n");       
         scanf("%hhd",&dev_id);
@@ -57,18 +54,19 @@ void func(int sockfd)
         arr[i]=dev_name;       //attribute id
         printf("Attribute id arr[%d] is %d\n",i,arr[i]);        //arr[2]
         i=i+1;
-        printf("value of i id %d\n",i);
+        printf("value of i is %d\n",i);
 
         printf("Enter the value to be inserted\n"); 
-        scanf("%s",name);
+        scanf("%s",&name);
         printf("Name is %s\n",name);
-        attr_len=strlen(name);
-       
+      
+        attr_len=strlen(name);      
         arr[i]=attr_len;                            //arr[3]
         printf("Attribute length of arr[%d] is %d\n",i,arr[i]);
         i=i+1;
         printf("value of i is %d\n",i);
-
+        
+        //i=sizeof(arr);
         i=strlen(arr);
         printf("value of i of array length is %d\n",i);
   
@@ -79,34 +77,11 @@ void func(int sockfd)
         //i=j;
         }
         m++;
-       // i=i+1;
-       // printf("i is %d",i);
-         
-        /*int z=attr_len+i;
-        for(int k=i;k<z;k++){
-       // strcpy(arr+i,name);
-        strcpy(arr,name);
-        printf("array of arr[%d] is %c\n",k,arr[i]);
-        }
-        i=k;
-        printf("i is %d\n",i);*/
+        write(sockfd,arr,sizeof(arr));
 
-          write(sockfd,arr,sizeof(arr));
-
-        } //end of case
-        break;
+        }
         
-        case 2: //remove a device
-        {
-        }
-         break;
-       
-        case 3: //edit the details of a device
-        {
-        }
-        break;
-        
-        }
+        } //end of for
         
 
 	/*char buff[MAX]; 
@@ -126,7 +101,7 @@ void func(int sockfd)
 			printf("Client Exit...\n"); 
 			break; 
 		} */
-	}
+	
 } 
 
 int main() 
